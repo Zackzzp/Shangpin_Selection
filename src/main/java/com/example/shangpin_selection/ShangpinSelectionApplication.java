e38eb7dc3e39f70186452ee80e32f4ad6c16bbda
zackzzp <1113739573@qq.com>
2024-10-17 17:17:09 +0800

    第一次提交

---
+++ src/main/java/com/example/shangpin_selection/ShangpinSelectionApplication.java
@@ -0,0 +1,13 @@
+package com.example.shangpin_selection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShangpinSelectionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShangpinSelectionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/shangpin_selection/ShangpinSelectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shangpin_selection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShangpinSelectionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
